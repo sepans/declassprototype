db8d5c1ef92d770aeaa0bdf83ebee95638edc4aa
sepans <sepans@sepans.com>
2015-06-13 17:04:50 -0400

    style montly-date misc

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,8 +17,8 @@
 function update(data, container, sumContainer) {
  // var data = randomizeData(20, Math.random()*100000);
   
-var margin = {top: 0, bottom: 20, left: 0, right: 0},
-      width = 1000,
+var margin = {top: 0, bottom: 50, left: 0, right: 0},
+      width = 600,
       height = 400,
       duration = 500,
       formatNumber = d3.format(',d'),
@@ -30,12 +30,18 @@ var margin = {top: 0, bottom: 20, left: 0, right: 0},
       h = height - margin.top - margin.bottom;
 
   var x = d3.scale.ordinal()
-              .rangeRoundBands([0, w], .1),
-      y = d3.scale.linear()
+              .rangeRoundBands([0, w], .1);
+
+  var y = d3.scale.linear()
               .range([h, 0]);
 
   y.domain([0, d3.max(data, function(d) { return d.y; })]);
+  
   x.domain(data.map(function(d) { return d.x; }));
+
+  console.log(x.domain(), d3.min(x.domain()), d3.max(x.domain()), x.range());
+
+
 
   var xAxis = d3.svg.axis()
                 .scale(x)
@@ -43,6 +49,20 @@ var margin = {top: 0, bottom: 20, left: 0, right: 0},
       yAxis = d3.svg.axis()
                 .scale(y)
                 .orient('left');
+
+  //display 15 chart labels at most.
+  if(data.length>15) {
+    var ratio = Math.ceil(data.length/20);
+    var ticks = data.reduce(function(prev, cur, i) {
+      if(i%ratio ===0) {
+        prev.push(cur.x);
+      }
+      return prev;
+
+    },[]);
+    xAxis.tickValues(ticks);
+
+  }
   
   var brush = d3.svg.brush()
                       .x(x)
@@ -90,17 +110,18 @@ var margin = {top: 0, bottom: 20, left: 0, right: 0},
               //var yValue = d.y
 
               tip
+                .html(d.hover ? d.hover : '<div class="title">'+xLabel+'</div>'+'<span><label>count: </label>'+d.y+'</span>');
+              tip.transition().duration(100)
                 .style('left',  x(d.x) + x.rangeBand()*0.83 + 'px')
                 .style('bottom', height -  y(d.y) + 20 + 'px')
-                .html(d.hover ? d.hover : '<div class="title">'+xLabel+'</div>'+'<span><label>count: </label>'+d.y+'</span>');
-
-               tip.style('opacity', 0.9);
+                .style('opacity', 0.9);
 
 
               
             })
             .on('mouseout', function(d) {
-                tip.style('opacity', 0);
+              tip.transition().duration(100)
+                .style('opacity', 0);
             })
           .selectAll('rect')
             .attr('height', h);
@@ -160,6 +181,11 @@ var margin = {top: 0, bottom: 20, left: 0, right: 0},
         .transition()
             .duration(duration)
               .call(yAxis);
+  chart.selectAll('.x .tick text')
+                .attr('transform', 'translate(0,0) rotate(20)')
+                .style('text-anchor', 'start');
+
+
 
   function brushstart() {
     chart.classed("selecting", true);
@@ -179,6 +205,8 @@ var margin = {top: 0, bottom: 20, left: 0, right: 0},
     var sumDiv = d3.select(sumContainer),
         extent = brush.extent(),
         sum = 0;
+
+        console.log('extent', extent, brush)
     
     data.forEach(function(d) {
       if (extent[0] <= x(d.x) && x(d.x) + x.rangeBand() <= extent[1])
@@ -215,32 +243,27 @@ var margin = {top: 0, bottom: 20, left: 0, right: 0},
 	    });
 	    update(personData, '#chart2', '#sum2');
 
-/*
-	    var dateData = _.map(json.date_data, function(item) {
+       
+      //montly
+	    var dateData = _.sortBy(_.map(json.date_data, function(item) {
 	    	return {
 	    		x: item.month,
 	    		y: item.doc_count
 	    	}
-	    });
-	    //console.log(data);
-	    update(dateData, '#chart2');
-	    */
-
-	    var dateData = _.groupBy(json.date_data, function(item) {
+	    }), 'x');
+	    update(dateData, '#chart3', '#sum3');
+	    
+     
+     
+/*
+	    // yearly
+      var dateData = _.groupBy(json.date_data, function(item) {
 	    	var year = item.month.split('-')[0];
 	    	return year;
 	    });
 
 	    dateData = _.mapValues(dateData, function(value) {
-/*
-	    	console.log('val ',value);
-	    	var sum = _.sum(value, function(item) {
-	    	console.log('item ',item);
-	    		return item.doc_count;
-	    	});
-	    	console.log('sum ',sum);
-	    	return sum;
-	    	*/
+
 	    
 	    	var sum = _.reduce(value, function(m, x) {
 	    		return m + x.doc_count;
@@ -251,20 +274,14 @@ var margin = {top: 0, bottom: 20, left: 0, right: 0},
 	    var newDateData = [];
 			Object.keys(dateData).forEach(function(key) {
 				newDateData.push({x: key, y: dateData[key]})
-			})    
-/*
-	   dateData = _.mapKeys(dateData, function(value, key) {
-	   	console.log(value, key);
-	   	return {
-	   		x: key,
-	   		y: value
+			});
 
-	   	}
-	   });
+         
+      update(newDateData, '#chart3', '#sum3');
+
 */
 
 
-	    update(newDateData, '#chart3', '#sum3');
 
 
 	    var topicData = _.map(json.topic_data, function(item) {
